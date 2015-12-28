61a67b8b6fb67d51bb69e894e0f1430e39ede8d8
Chris Alfano <chris@jarv.us>
2015-12-27 20:56:44 -0500

    Finish handling setup of remote user on backup host

--- setup.js
+++ setup.js
@@ -8,13 +8,16 @@ var winston = require('winston'),
 // paths
 var backupServicePath = '/emergence/services/backup',
     configPath = backupServicePath + '/config.json',
-    privateKeyPath = backupServicePath + '/id_rsa';
+    privateKeyPath = backupServicePath + '/id_rsa',
+    publicKeyPath = backupServicePath + '/id_rsa.pub';
 
 // ensure backup service isn't already configured
 if (fs.existsSync(backupServicePath)) {
     winston.error(backupServicePath + ' already exists');
     process.exit(1);
 }
+
+// TODO: use async.auto?
 
 prompt.start();
 
@@ -40,80 +43,65 @@ prompt.get([{
     winston.info('Creating SSH connection to '+result.host+'...');
     var ssh = sequest.connect(result);
 
-    winston.info('Checking if user exists...');
-    ssh('getent passwd ' + result.backup_username + '> /dev/null; echo $?', function(error, output, info) {
+    winston.info('Reading backup server configuration...');
+    ssh('cat /etc/emergence-backup.json || echo ""', function(error, output, info) {
+        var serverConfig;
+
         if (error) {
             winston.error('Failed to connect to backup host', error);
             ssh.end();
             process.exit(2);
         }
 
-        if (output.trim() == '0') {
-            winston.error('Username "%s" already exists on %s', result.backup_username, result.host);
+        if (!output.trim() || !(serverConfig = JSON.parse(output))) {
+            winston.error('Failed to read /etc/emergence-backup.json on backup host', error);
             ssh.end();
             process.exit(3);
         }
 
-        ssh.end();
+        winston.info('serverConfig', serverConfig);
 
-        winston.info('Creating ' + backupServicePath + '...');
-        fs.mkdirSync(backupServicePath, '700');
+        winston.info('Checking if user exists...');
+        ssh('getent passwd ' + result.backup_username + '> /dev/null; echo $?', function(error, output, info) {
 
-        winston.info('Generating ' + privateKeyPath + '...');
-        execSync.exec('ssh-keygen -t rsa -N "" -f ' + privateKeyPath);
-
-        winston.info('Writing config to ' + configPath + '...');
-        fs.writeFileSync(configPath, JSON.stringify({
-            host: result.host,
-            mysql: {
-                ignoreTables: '*.sessions'
+            if (output.trim() == '0') {
+                winston.error('Username "%s" already exists on %s', result.backup_username, result.host);
+                ssh.end();
+                process.exit(4);
             }
-        }, null, 4));
 
-        fs.chmodSync(configPath, '600');
+            winston.info('Creating ' + backupServicePath + '...');
+            fs.mkdirSync(backupServicePath, '700');
+
+            winston.info('Generating ' + privateKeyPath + '...');
+            execSync.exec('ssh-keygen -t rsa -N "" -f ' + privateKeyPath);
+
+            var username = result.backup_username,
+                home = serverConfig.homePrefix + '/' + username,
+                setupCmd = [
+                    'sudo useradd -m -s /bin/bash -d ' + home + ' ' + username,
+                    'sudo mkdir ' + home + '/.ssh',
+                    'sudo chmod 700 ' + home + '/.ssh',
+                    'echo "' + fs.readFileSync(publicKeyPath) + '" | sudo tee ' + home + '/.ssh/authorized_keys',
+                    'sudo chmod 600 ' + home + '/.ssh/authorized_keys',
+                    'sudo chown ' + username + ':' + username + ' -R ' + home
+                ];
+
+            winston.info('Creating user %s with home %s...', username, home);
+            ssh(setupCmd.join(' && '), function(error, output, info) {
+                ssh.end();
+
+                winston.info('Writing config to ' + configPath + '...');
+                fs.writeFileSync(configPath, JSON.stringify({
+                    host: result.host,
+                    user: username,
+                    mysql: {
+                        ignoreTables: '*.sessions'
+                    }
+                }, null, 4));
+
+                fs.chmodSync(configPath, '600');
+            });
+        });
     });
 });
-
-/*
-var fs = require('fs'),
-    async = require('async'),
-    rsync = require('rsyncwrapper').rsync;
-
-
-// configure logger
-if (!fs.existsSync('/var/log/emergence-backup')){
-    fs.mkdirSync('/var/log/emergence-backup');
-}
-
-winston.add(winston.transports.DailyRotateFile, {
-    level: 'verbose',
-    filename: '/var/log/emergence-backup/log'
-});
-
-
-// load config
-winston.info('Loading config...');
-var config = JSON.parse(fs.readFileSync('/etc/mrbackup/config.json'));
-winston.info('Loaded config:', config);
-
-
-winston.info('Creating SSH connection...');
-var ssh = sequest.connect({
-    host: config.host,
-    username: config.user,
-    privateKey: fs.readFileSync('/etc/mrbackup/id_rsa')
-});
-
-
-winston.info('Executing backup...');
-
-async.auto({
-    getHome: function(callback) {
-        winston.info('Checking home directory...');
-        ssh('echo $HOME', function(error, output, info) {
-            var home = output.trim(0);
-            winston.info('Remote home directory:', home);
-            callback(null, home);
-        });
-    },
-*/
